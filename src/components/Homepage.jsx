9b5d0454f36bc3deb894cc41feee9f442d5fdb6f
Nino Akut <akutnino@gmail.com>
2024-07-23 02:57:01 +0800

    added Homepage semantic elements and added my hero photo

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom';
 import styles from './../styles/Homepage.module.scss';
 
 export default function Homepage() {
 	return (
 		<div className={styles.main}>
-			<div className={styles.container}></div>
+			<div className={styles.container}>
+				<header>
+					<nav>@akutnino</nav>
+					<h1>
+						Nice to meet you! I&apos;m <span>Niño Akut</span>.
+					</h1>
+					<p>
+						Based in the Philippines, I&apos;m a ReactJS Web Developer that&apos;s
+						passionate about building web apps that users love.
+					</p>
+					<Link to={'/resume'}>Check My Resume</Link>
+					<img
+						src='/src/assets/images/IMG_20240723_013808.jpg'
+						alt='my-photo'
+					/>
+				</header>
+				<section></section>
+				<section></section>
+				<footer></footer>
+			</div>
 		</div>
 	);
 }
