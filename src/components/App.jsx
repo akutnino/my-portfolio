c2fcc6b5d37e1f91763483df14f728f0791ad5f7
Nino Akut <akutnino@gmail.com>
2024-07-23 02:09:11 +0800

    created Homepage and Resume components and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './Homepage';
+import Resume from './Resume';
+
 export default function App() {
-	return <div>Demo</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					element={<Homepage />}
+				/>
+				<Route
+					path='resume'
+					element={<Resume />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
