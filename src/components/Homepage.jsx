b33d1e6ad1600b9f38a33ec171f28b0d2abb8f62
Nino Akut <akutnino@gmail.com>
2024-07-23 02:24:51 +0800

    added global styles in index.scss

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,3 +1,9 @@
+import styles from './../styles/Homepage.module.scss';
+
 export default function Homepage() {
-	return;
+	return (
+		<div className={styles.main}>
+			<div className={styles.container}></div>
+		</div>
+	);
 }
