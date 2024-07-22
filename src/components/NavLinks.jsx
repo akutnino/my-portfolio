a7ea554cbb16d1a77864923edf4e50a23cedd312
Nino Akut <akutnino@gmail.com>
2024-07-23 03:39:53 +0800

    added all links in NavLinks component

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -13,6 +13,42 @@ export default function NavLinks() {
 					/>
 				</a>
 			</li>
+			<li>
+				<a
+					href='https://www.frontendmentor.io/profile/akutnino'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						src='/src/assets/images/icon-frontend-mentor.svg'
+						alt='frontend-mentor'
+					/>
+				</a>
+			</li>
+			<li>
+				<a
+					href='https://ph.linkedin.com/in/nino-akut'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						src='/src/assets/images/icon-linkedin.svg'
+						alt='linkedin'
+					/>
+				</a>
+			</li>
+			<li>
+				<a
+					href='https://x.com/akutnino?lang=en'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						src='/src/assets/images/icon-twitter.svg'
+						alt='twitter'
+					/>
+				</a>
+			</li>
 		</ul>
 	);
 }
