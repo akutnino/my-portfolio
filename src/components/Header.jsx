11d98a808d0612fe55ed821e203357a2dd89b412
Nino Akut <akutnino@gmail.com>
2024-07-23 18:07:02 +0800

    styled the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,33 @@
 import { Link } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import styles from '../styles/Header.module.scss';
 
 export default function Header() {
 	return (
-		<header>
+		<header className={styles.header}>
 			<nav>
 				@akutnino
 				<NavLinks />
 			</nav>
-			<h1>
-				Nice to meet you! I&apos;m <span>Niño Akut</span>.
-			</h1>
-			<p>
-				Based in the Philippines, I&apos;m a ReactJS Web Developer that&apos;s passionate
-				about building web apps that users love.
-			</p>
+			<div>
+				<h1>
+					Nice to meet you! I&apos;m <span>Niño Akut</span>.
+				</h1>
+				<p>
+					Based in the Philippines, I&apos;m a ReactJS Web Developer that&apos;s
+					passionate about building web apps that users love.
+				</p>
+			</div>
 			<img
 				src='/src/assets/images/IMG_20240723_013808.jpg'
 				alt='my-photo'
 			/>
-			<Link to={'/resume'}>Check My Resume</Link>
+			<Link
+				to={'/resume'}
+				className={styles.resume}
+			>
+				Check My Resume
+			</Link>
 		</header>
 	);
 }
