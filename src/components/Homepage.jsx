57d1a9cc9c8a85a5cd345a9e0bc9f650b406cca8
Nino Akut <akutnino@gmail.com>
2024-07-24 19:17:13 +0800

    created TechStack component and added its styles

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,13 @@
 import styles from './../styles/Homepage.module.scss';
 import Header from './Header';
+import TechStack from './TechStack';
 
 export default function Homepage() {
 	return (
 		<div className={styles.main}>
 			<div className={styles.container}>
 				<Header />
-				<section></section>
+				<TechStack />
 				<section></section>
 				<footer></footer>
 			</div>
---
+++ src/components/TechStack.jsx
@@ -0,0 +1,32 @@
+import styles from '../styles/TechStack.module.scss';
+
+export default function TechStack() {
+	return (
+		<section className={styles.techStackSection}>
+			<div>
+				<h1>HTML</h1>
+				<p>4 Years Experience</p>
+			</div>
+			<div>
+				<h1>CSS</h1>
+				<p>4 Years Experience</p>
+			</div>
+			<div>
+				<h1>JavaScript</h1>
+				<p>2 Years Experience</p>
+			</div>
+			<div>
+				<h1>React</h1>
+				<p>&lt; 1 Year Experience</p>
+			</div>
+			<div>
+				<h1>React Router</h1>
+				<p>&lt; 1 Year Experience</p>
+			</div>
+			<div>
+				<h1>Sass</h1>
+				<p>&lt; 1 Year Experience</p>
+			</div>
+		</section>
+	);
+}
